21950cb7dd8dc8816948855beab2e75bfbc6ef48
Your Name <you@example.com>
2023-01-19 14:35:26 -0800

    Used concrete type for Pull request object, added unit test for an actual experimenting with creating pull request

---
+++ NugetUpdate/WorkItems/PullRequestItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NugetPackageUpdates.WorkItems
+{
+    public class PullRequestItem
+    {
+
+        public int PullRequestId { get; set; }
+        public string ArtifactId { get; set; }
+
+        public CreatedBy CreatedBy { get; set; }
+    }
+}
